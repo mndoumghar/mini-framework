11467c0c71810a5dedce0c6adbe9fa69cdd036c6
mndoumghar <mohmamedndoumghar@gmail.com>
2025-11-29 14:52:04 +0100

    ADD method creatRealElemnt => class VirtualDOM

--- framework/VirtualDOM.js
+++ framework/VirtualDOM.js
@@ -1,16 +1,19 @@
 
-
 export class VirtualDOM {
     constructor() {}
+    render(countairnt) {
+        countairnt.innerHTML = '';
+    }
     createVirtualNode(node) {
-        if(typeof node === "string" || typeof node === "number") {
+
+        if (typeof node === "string" || typeof node === "number") {
             return {
-                type: "TEXT_NODE" ,
+                type: "TEXT_NODE",
                 value: String(node)
             }
         }
 
-        if(!node || !node.tag) {
+        if (!node || !node.tag) {
             return {
                 type: 'TEXT_NODE',
                 value: ''
@@ -18,14 +21,46 @@ export class VirtualDOM {
         }
 
         return {
-            type: "ELEMENT_NODE" ,
+            type: "ELEMENT_NODE",
             tage: node.tag,
-            attrs:node.attrd || {} ,
+            attrs: node.attrs || {},
             children: node.children
-            .flat()
-            .filter(res => res != null && res!= undefined && res != false)
-            .map(res => this.createVirtualNode(res))
-        }        
+                .flat()
+                .filter(res => res != null && res != undefined && res != false)
+                .map(res => this.createVirtualNode(res))
+        }
     }
 
+    creatRealElemnt(VNode) {
+        if (VNode.type === "TEXT_NODE") {
+            return document.createTextNode(VNode.value)
+        }       
+        
+        const element = document.createElement(VNode.tag);
+        const attrArray = Object.entries(VNode.attrs || {});
+
+        for (let [name, value] of attrArray) {
+    
+            if (name.includes('on') && typeof value === "function") {
+                element[name.toLocaleLowerCase()] = value
+        
+            } else if (name === "selected" || name === "checked") {
+                element[name] = Boolean(value)
+        
+            } else if (name === "autoFocus") {
+                if (value) {
+                    setTimeout(() => element.focus(), 0)
+        
+                } else {
+                    element.setAttribut(name, value)
+                }
+            }
+        }
+
+        for (let child of VNode.children) {
+            element.appendChild(this.creatRealElemnt(child));
+        }
+
+        return element
+    }
 }
